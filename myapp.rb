9d2fd0cd3d95d352a3abdf3aaa7ec0c0115dd998
koyo-miyamura <nexus9071@gmail.com>
2018-04-03 19:19:58 +0900

    @contentsの順番を最新順に変更、index.erbのバグ修正

--- myapp.rb
+++ myapp.rb
@@ -22,9 +22,22 @@ class MyApp < Sinatra::Base
   end
 
   get '/' do
-    sql = "select contents.id as content_id, image_path, caption, users.name as name from contents inner join users on contents.user_id = users.id limit 50"
+    sql = "
+          SELECT contents.id AS content_id, image_path, caption, users.name AS name, contents.created_at AS content_created_at 
+          FROM contents
+          INNER JOIN users
+          ON contents.user_id = users.id
+          ORDER BY contents.created_at
+          LIMIT 50
+          "
     contents_users = $client.query(sql)
-    sql = "select text, content_id, users.name as name from comments inner join users on comments.user_id = users.id"
+    #content_ids = contents_users.each
+    sql = "
+          SELECT text, content_id, users.name AS name, comments.created_at AS comment_created_at
+          FROM comments
+          INNER JOIN users
+          ON comments.user_id = users.id
+          "
     comments_users = $client.query(sql)
     @contents = []
     # contents-users relation
@@ -37,19 +50,26 @@ class MyApp < Sinatra::Base
             user: {
               name: comm_u["name"] 
             },
-            text: comm_u["text"]
+            text: comm_u["text"],
+            comment_created_at: comm_u["comment_created_at"]
           })
         end
       end
+      # ASC by comment_created_at
+      # [{..., comment_created_at: 2018-01-01 00:00:00 +0900}, {..., comment_created_at: 2018-04-03 18:44:04 +0900}] 
+      comments_users.sort_by {|hash| hash[:comment_created_at]}
       @contents.push({
         image_path: cont_u["image_path"],
         caption:    cont_u["caption"],
+        content_created_at: cont_u["content_created_at"],
         user: {
           name: cont_u["name"]
         },
         comments: contents_comments_users
       })
     end
+    # ASC by content_created_at
+    @contents.sort_by {|hash| hash[:content_created_at]}
     erb :index
   end
   
